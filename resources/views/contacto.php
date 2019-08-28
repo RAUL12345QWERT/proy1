c8b9923a4adc1e8c611b0c08982cc6f50f8304f8
RAUL12345QWERT <vladimircalle@outlook.com.ar>
2019-08-28 11:21:04 -0400

    tercer commit

---
+++ resources/views/contacto.php
@@ -0,0 +1,15 @@
+@extends('layouts.principal')
+@include('alerts.errors')
+@include('alerts.request')
+@section('content')
+    <div class="header">
+        <div class="top-header">
+            <div class="logo">
+                <a href="#"><img src="../public/images/bd8.png" alt=""></a>
+                <p>movie theater</p>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+
+        
+    </div>
